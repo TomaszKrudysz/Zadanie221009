2535ca25df40449ea0a4a90e177b3220dddbb7a5
Tomasz Krudysz <t.krudysz@gmail.com>
2022-10-12 12:49:37 +0200

    Initial commit

---
+++ src/Zadanie1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * Created by tkrud on 12.10.2022.
+ *
+ * Napisz program, w którym zadeklarujesz i utworzysz dziesięcioelementową tablicę
+ * odpowiedniego typu. W pętli pobierzesz od użytkownika liczby i je w niej zapiszesz.
+ * Następnie wyświetl je na ekranie powiadomienia "Liczba: " dla każdego z podanych
+ * parametrów.
+ */
+public class Zadanie1 {
+    public static void main(String[] args) {
+        int[] tab = new int[10];
+        Scanner scan = new Scanner(System.in);
+        for (int i=0; i<10;i++ ) {
+            System.out.println("Podaj liczbe nr: " + (i + 1) + " z " + tab.length);
+            tab[i] = scan.nextInt();
+        }
+        for (int x: tab){
+            System.out.println("Liczba: " + x);
+        }
+    }
+}
